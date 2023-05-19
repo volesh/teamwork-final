aad3c7cbedb09a5acc33d72df731e02c76bae8ea
volesh <volesh2@gmail.com>
2023-05-19 14:52:34 +0300

    fix

--- src/middlewares/timely.middlewares.ts
+++ src/middlewares/timely.middlewares.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Response } from "express";
 import { timelyService } from "../services";
 import { IRequest } from "../interfaces";
-import { promises as fs } from "fs";
+// import { promises as fs } from "fs";
 
 export const timelyMiddlewares = {
   createProject: async (req: IRequest, res: Response, next: NextFunction) => {
@@ -209,8 +209,8 @@ export const timelyMiddlewares = {
     try {
       const code = req.query.code as string;
       const { data } = await timelyService.getTokens(code);
-      await fs.writeFile("./src/tokens.json", JSON.stringify(data));
       console.log(data);
+      // await fs.writeFile("./src/tokens.json", JSON.stringify(data));
       next();
     } catch (e) {
       next(e);
