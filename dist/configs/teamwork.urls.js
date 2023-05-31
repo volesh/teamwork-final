a39a83f0362760b7df28e93d3c1c985eb648f0cf
VladPron <vladislavpron0@gmail.com>
2023-05-31 14:20:53 +0300

    dfs

---
+++ dist/configs/teamwork.urls.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.teamworkUrls = void 0;
+exports.teamworkUrls = {
+    people: "/people",
+    projects: "/projects",
+};
+//# sourceMappingURL=teamwork.urls.js.map
---
+++ dist/configs/timelyapp.urls.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.timelyUrls = void 0;
+exports.timelyUrls = {
+    version: "/1.1",
+    accounts: "/accounts",
+    projects: "/projects",
+    clients: "/clients",
+    currentUser: "/users/current",
+    tokens: "/oauth/token",
+    users: "/users",
+};
+//# sourceMappingURL=timelyapp.urls.js.map
